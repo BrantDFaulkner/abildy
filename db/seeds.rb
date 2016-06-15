2ad853ddfd6cc22bda1b163606012b0059fdd55b
Brant Faulkner <brantdfaulkner@gmail.com>
2016-06-15 15:41:38 -0500

    add seeds for users, goals, requests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
+# Seed database with fixed tables
 category_titles = %w[Financial Intellectual Family Spiritual Physical]
 statuses = %w[Pending Approved Declined]
 
@@ -18,4 +11,27 @@ statuses.each do |title|
   Status.create!(title: title)
 end
 
-User.create!(user_name: "brang", email: "brang@brang.com", password: "password", )
+# Create Primary User
+brang = User.create!(user_name: "brang", email: "brang@brang.com", password: "password")
+brang.goals.create!(admin_id: brang.id,
+  category_id: 1,
+  title: "Brang's Goal",
+  description: "Brang loves to make goals and acheive them with his fellow abildians!",
+  )
+
+10.times do
+  user = User.create!(user_name: Faker::Name.name, email: Faker::Internet.email, password: Faker::Internet.password)
+  Goal.all.each do |goal|
+    Request.create!(
+      goal_id: goal.id,
+      user_id: user.id,
+      status_id: 1,
+    )
+  end
+  Goal.create!(
+    admin_id: user.id,
+    category_id: rand(1..5),
+    title: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph,
+  )
+end
