2db67cc87c219226c2224ddff1030cce0dcc322b
Brant Faulkner <brantdfaulkner@gmail.com>
2016-06-15 16:23:01 -0500

    remove pry from master

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,8 +1,6 @@
 class ParticipationsController < ApplicationController
 
   def create
-    require 'pry'
-    binding.pry
     Participation.create(user_id: params[:request_user_id].to_i, goal_id: session[:goal_id])
     Request.find_by_id(params[:request_id].to_i).update(status_id: 2)
     redirect_to :back
